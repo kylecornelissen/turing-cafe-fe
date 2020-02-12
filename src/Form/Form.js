24d00fed82039ae87dbd687d0a1a4aa7b9081526
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-12 10:26:32 -0700

    Add Form for adding reservations

---
+++ src/Form/Form.js
@@ -0,0 +1,70 @@
+// Create a controlled form component to create a reservation
+// (look into the API documentation to see what pieces of information are expected,
+// and what datatypes it wants each piece to be).
+// As a user types into the input fields, the component's state should change.
+// When the user clicks the Make Reservation button, the application should update the App's state.
+// The new reservation should display with all of the existing reservations.
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      num: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+  submitReservation = (e) => {
+    const {name, date, time, num} = this.state;
+    const newReservation = {name, date, time, num};
+    const {addReservation} = this.props;
+    addReservation(newReservation);
+    this.clearForm();
+  }
+  clearForm() {
+    this.setState({name: '', date: '', time: '', num: ''});
+  }
+  render() {
+    return (
+      <form autoComplete="off">
+        <h1>Create Reservation Form</h1>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='How many guests?'
+          name='num'
+          value={this.state.num}
+          onChange={event => this.handleChange(event)}
+        />
+        <button type="submit" onClick={(e) => this.submitReservation(e)}>Make Reservation</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
