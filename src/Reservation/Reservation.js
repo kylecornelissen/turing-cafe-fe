fcc41b40f880dca63c72412a25f09ac7515b7aee
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-12 10:27:34 -0700

    Add reservation card styling for better visual

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Reservation.css';
 
 const Reservation = ({key, name, date, time, num}) => {
   return (
-    <article className="reservation-card" key={key}>
+    <article className="reservation-card">
       <h1>{name}</h1>
       <h2>{date}</h2>
       <h2>{time} pm</h2>
